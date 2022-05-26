4dabf7e7ef973c3c4f6090f7b87d9e40dbf475aa
Mhadhbi Issam <95243282+MhadhbiXissam@users.noreply.github.com>
2022-05-26 17:52:56 +0100

    v1.4

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
-
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(
-    name='Html2Js',
-    version='1.0',
+    name='Html2js',
+    version='1.4',
     license='MIT',
     author="Mhadhbi Issam",
     author_email='mhadhbixissam@gmail.com ',
@@ -14,5 +16,7 @@ setup(
     install_requires=[
           'beautifulsoup4','lxml'
       ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 
 )
